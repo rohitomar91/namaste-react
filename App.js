285a6e8777d05f80fc698a69706d09ae47401d71
Rohit Omar <rohitomar@Rohits-MacBook-Air.local>
2023-06-12 23:07:10 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,30 @@
+const content = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "heading", key: "1" },
+      "Hello child 1 - 1!"
+    ),
+    React.createElement(
+      "h1",
+      { id: "heading", key: "2" },
+      "Hello child 1 - 2!"
+    ),
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement(
+      "h1",
+      { id: "heading", key: "1" },
+      "Hello child 2 - 1!"
+    ),
+    React.createElement(
+      "h1",
+      { id: "heading", key: "2" },
+      "Hello child 2 - 2!"
+    ),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(content);
