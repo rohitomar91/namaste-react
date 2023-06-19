6b6812b30e85a4ff72ba46baa331a3dd90de0b75
Rohit Omar <rohitomar@Rohits-MacBook-Air.local>
2023-06-20 01:05:32 +0530

    Episode 04

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import resData from "./data.json";
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    This is JSX Heading 🚀
-  </h1>
-);
-
-const HeadingComponent = () => (
-  // <> </> know as React Fragments
-  // We can write it like this also <React.Fragment></React.Fragment>
-  <>
-    <div className="container">
-      <Title />
-      <h1 className="heading">This is A Heading Component🚀</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.designevo.com/res/templates/thumb_small/separated-brown-burger.webp"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    <div className="container-2"></div>
-  </>
-);
+  );
+};
+
+const RestaurantCard = (props) => {
+  const {
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+    cloudinaryImageId,
+  } = props.resData?.data;
+  return (
+    <div className="res-card">
+      <img
+        alt="res-logo"
+        className="res-logo"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <p>{cuisines.join(", ")}</p>
+      <p>{avgRating} stars</p>
+      <p>{deliveryTime} mins</p>
+      <p>₹ {costForTwo / 100} For Two</p>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resData.cards.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
